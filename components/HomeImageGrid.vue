20be96e2f2c23fa78fa5f407c3855979a4630792
alexjoita <alex@alexjoita.com>
2025-09-05 19:20:02 +0300

    Add some photos, minor improvments

--- components/HomeImageGrid.vue
+++ components/HomeImageGrid.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="flex items-center justify-center gap-5 py-4 sm:gap-6 relative z-20">
     <!-- col1 -->
-    <div class="hidden lg:flex flex-col gap-5 sm:gap-6 " v-parallax data-rellax-speed="3">
+    <div class="hidden lg:flex flex-col gap-5 sm:gap-6 " v-parallax data-rellax-speed="8">
       <div
         class="relative aspect-[2/3] w-36 lg:w-52 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800"
       >
@@ -20,7 +20,7 @@
     </div>
 
     <!-- col2 -->
-    <div class="flex flex-col gap-5 sm:gap-6" v-parallax data-rellax-speed="1">
+    <div class="flex flex-col gap-5 sm:gap-6" v-parallax data-rellax-speed="5">
       <div
         class="relative aspect-[4/3] w-44 md:w-52 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800"
       >
